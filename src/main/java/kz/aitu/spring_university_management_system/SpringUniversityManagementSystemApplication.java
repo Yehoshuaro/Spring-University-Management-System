24f34096db11be969d880f3db1a8181776556aa5
Yehoshuaro <231132@astanait.edu.kz>
2024-02-24 23:44:11 +0600

    First steps in the Assignment

---
+++ src/main/java/kz/aitu/spring_university_management_system/SpringUniversityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.spring_university_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUniversityManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUniversityManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aitu/spring_university_management_system/SpringUniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.spring_university_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUniversityManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
