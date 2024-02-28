6d0ef0f56b81434848d2fbfc39c7aac8ec69d989
Аскат Наринбетов <231308@astanait.edu.kz>
2024-02-29 03:03:29 +0600

    StudentServiceInterface and methods(getAllStudent,DeadSouls,Hungergames) and I think will add 1 more method to show GPA

---
+++ src/main/java/kz/aitu/spring_university_management_system/services/interfaces/StudentServiceInterface.java
@@ -0,0 +1,13 @@
+package kz.aitu.spring_university_management_system.services.interfaces;
+
+import kz.aitu.spring_university_management_system.models.Student;
+
+import java.util.List;
+
+public interface StudentServiceInterface {
+    List<Student> getAllStudent(); // to get all students
+    boolean DeadSouls(int studentId); // To check if Attendance less than 70% and show if u have a retake or no
+    double Hungergames(int studentId); // to check if the student has a Scholarship,
+                                            // IncreasedScholarship or No Scholarship
+
+}
