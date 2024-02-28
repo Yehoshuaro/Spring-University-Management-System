2ebe136a242b7849e20f12545c61db0fd0c80ce9
Nuris <230445@astanait.edu.kz>
2024-02-29 01:26:18 +0600

    added toStiring  teacher for UserService

--- src/main/java/kz/aitu/spring_university_management_system/models/Teacher.java
+++ src/main/java/kz/aitu/spring_university_management_system/models/Teacher.java
@@ -24,4 +24,23 @@ public class Teacher extends User {
     public void addAssignment(Assignment assignment) {
         assignments.add(assignment);
     }
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Teacher Details:\n");
+        stringBuilder.append("Name: ").append(getName()).append("\n");
+        stringBuilder.append("Surname: ").append(getSurname()).append("\n");
+        stringBuilder.append("Age: ").append(getAge()).append("\n");
+        stringBuilder.append("Course: ").append(course).append("\n");
+        stringBuilder.append("Assignments:\n");
+
+        for (Assignment assignment : assignments) {
+            stringBuilder.append("- Title: ").append(assignment.getTitle())
+                    .append(", Description: ").append(assignment.getDescription())
+                    .append(", Due Date: ").append(assignment.getDueDate())
+                    .append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
