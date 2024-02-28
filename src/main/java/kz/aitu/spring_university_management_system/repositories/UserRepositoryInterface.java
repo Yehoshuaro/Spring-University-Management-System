dd5eb5b2592960610b61e5e26dca33c15547fecc
Аскат Наринбетов <231308@astanait.edu.kz>
2024-02-29 03:13:24 +0600

    I used Hungergames and DeadSouls methods in class StudentService

--- src/main/java/kz/aitu/spring_university_management_system/repositories/UserRepositoryInterface.java
+++ src/main/java/kz/aitu/spring_university_management_system/repositories/UserRepositoryInterface.java
@@ -1,10 +1,14 @@
 package kz.aitu.spring_university_management_system.repositories;
 
 import kz.aitu.spring_university_management_system.models.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserRepositoryInterface extends JpaRepository<User,Integer> {
     List<User> findBySurname(String surname);
+
+
+
 }
