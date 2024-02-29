15531059e53e1e3aa23ff265f08be084680bc997
Аскат Наринбетов <231308@astanait.edu.kz>
2024-02-29 09:21:19 +0600

    ad

--- src/main/java/kz/aitu/spring_university_management_system/controllers/UserController.java
+++ src/main/java/kz/aitu/spring_university_management_system/controllers/UserController.java
@@ -9,17 +9,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("user_table")
+@RequestMapping
 public class UserController {
     private final UserServiceInterface service;
 
     public UserController(UserServiceInterface service) {
         this.service = service;
-    }
-
-    @GetMapping("hello")
-    public String sayHello() {
-        return "Алматынын тундери ай";
     }
 
     @GetMapping("/")
@@ -31,8 +26,8 @@ public class UserController {
     public ResponseEntity<User> getById(@PathVariable("user_id") int id) {
         User user = service.getById(id);
         if (user == null)
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND); //404 error
-        return new ResponseEntity<>(user, HttpStatus.OK); //200 error
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/")
@@ -42,7 +37,7 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED); //201
     }
-
+    //da
     @GetMapping("/surname/{user_surnme}")
     public List<User> getAllBySurname(@PathVariable("user_surname") String surname) {
         return service.getBySurname(surname);
