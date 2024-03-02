b15d76f61a670ba2cf1a93c952b7b820c88f7b68
Yehoshuaro <231132@astanait.edu.kz>
2024-03-02 09:30:50 +0500

    Checking, because commits are not working

--- src/main/java/kz/aitu/spring_university_management_system/services/TeacherService.java
+++ src/main/java/kz/aitu/spring_university_management_system/services/TeacherService.java
@@ -2,36 +2,45 @@ package kz.aitu.spring_university_management_system.services;
 
 import kz.aitu.spring_university_management_system.models.Assignment;
 import kz.aitu.spring_university_management_system.models.Teacher;
+import kz.aitu.spring_university_management_system.services.interfaces.TeacherServiceInterface;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class TeacherService {
+public class TeacherService implements TeacherServiceInterface {
 
 
     private Teacher currentTeacher;
 
+    @Override
     public Teacher getCurrentTeacher() {
         return currentTeacher;
     }
 
+    @Override
     public void setCurrentTeacher(Teacher teacher) {
         this.currentTeacher = teacher;
     }
 
+    @Override
     public void updateCourse(String newCourse) {
-        currentTeacher.setCourse(newCourse);
+        if (currentTeacher != null) {
+            currentTeacher.setCourse(newCourse);
+        }
     }
 
+    @Override
     public void addAssignment(String title, String description, String dueDate) {
-        Assignment assignment = new Assignment(title, description, dueDate);
-        currentTeacher.addAssignment(assignment);
+        if (currentTeacher != null) {
+            Assignment assignment = new Assignment(title, description, dueDate);
+            currentTeacher.addAssignment(assignment);
+        }
     }
 
+    @Override
     public char calculateGrade(int studentScore) {
-
         if (studentScore >= 90) {
             return 'A';
         } else if (studentScore >= 80) {
