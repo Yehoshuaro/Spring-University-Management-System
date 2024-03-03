14d73cea3cca588e0d2c7f33cce065cd8752d6b9
Yehoshuaro <231132@astanait.edu.kz>
2024-03-03 20:16:19 +0500

    New commit

--- src/main/java/kz/aitu/spring_university_management_system/services/TeacherService.java
+++ src/main/java/kz/aitu/spring_university_management_system/services/TeacherService.java
@@ -1,39 +1,52 @@
 package kz.aitu.spring_university_management_system.services;
 
-import kz.aitu.spring_university_management_system.models.Assignment;
 import kz.aitu.spring_university_management_system.models.Teacher;
-import lombok.Getter;
-import lombok.Setter;
+import kz.aitu.spring_university_management_system.repositories.TeacherRepositoryInterface;
+import kz.aitu.spring_university_management_system.services.interfaces.TeacherServiceInterface;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
-@Setter
-@Getter
 @Service
-public class TeacherService {
-    private Teacher currentTeacher;
+public class TeacherService implements TeacherServiceInterface {
 
-    public void updateCourse(String newCourse) {
-        currentTeacher.setCourse(newCourse);
+    private final TeacherRepositoryInterface repo;
+
+    public TeacherService(TeacherRepositoryInterface repo) {
+        this.repo = repo;
     }
 
-    public void addAssignment(String title, String description, String dueDate) {
-        Assignment assignment = new Assignment(title, description, dueDate);
-        currentTeacher.addAssignment(assignment);
+    @Override
+    public List<Teacher> getCurrentTeacher() {
+        return repo.findAll();
     }
 
-    public char calculateGrade(int studentScore) {
-
-        if (studentScore >= 90) {
-            return 'A';
-        } else if (studentScore >= 80) {
-            return 'B';
-        } else if (studentScore >= 70) {
-            return 'C';
-        } else if (studentScore >= 60) {
-            return 'D';
-        } else {
-            return 'F';
-        }
+    @Override
+    public Teacher currentTeacherById(int teacher_id) {
+        return repo.findById(teacher_id).orElse(null);
     }
+
+//    public void updateCourse(String newCourse) {
+//        currentTeacher.setCourse(newCourse);
+//    }
+//
+//    public void addAssignment(String title, String description, String dueDate) {
+//        Assignment assignment = new Assignment(title, description, dueDate);
+//        currentTeacher.addAssignment(assignment);
+//    }
+//
+//    public char calculateGrade(int studentScore) {
+//
+//        if (studentScore >= 90) {
+//            return 'A';
+//        } else if (studentScore >= 80) {
+//            return 'B';
+//        } else if (studentScore >= 70) {
+//            return 'C';
+//        } else if (studentScore >= 60) {
+//            return 'D';
+//        } else {
+//            return 'F';
+//        }
+//    }
 }
